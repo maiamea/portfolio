be9ce433d8ced77a7f25a4512c70a99d1af94e21
maiamea <maiameawork@gmail.com>
2021-10-24 18:01:18 +0900

    publish: HelloPage更新

--- docs/component---src-pages-hello-jsx-55cec26b7b24fdae1078.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmaiamea_s_portfolio=self.webpackChunkmaiamea_s_portfolio||[]).push([[540],{2760:function(e,l,t){t.r(l);var a=t(7294);l.default=function(){return a.createElement("main",null,a.createElement("h1",null,"Hello, World!"),a.createElement("p",{style:{color:"red"}},"こんにちは"))}}}]);
-//# sourceMappingURL=component---src-pages-hello-jsx-55cec26b7b24fdae1078.js.map
