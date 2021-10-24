fbb817858fe377f248bc783aff0262f26cb2e642
maiamea <maiameawork@gmail.com>
2021-10-24 15:31:35 +0900

    add HelloPage

---
+++ src/pages/hello.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const HelloPage = () => {
+    return (
+        <main>
+            <h1>Hello, World!</h1>
+            <p style={
+                {color:"red"}
+            }>こんにちは</p>
+        </main>
+    )
+}
+
+export default HelloPage
