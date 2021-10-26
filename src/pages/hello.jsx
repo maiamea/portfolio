22f12e343ed5cc425c1dea6a2ba552937cbb620f
maiamea <maiameawork@gmail.com>
2021-10-26 15:52:59 +0900

    add ExampleComponent

--- src/pages/hello.jsx
+++ src/pages/hello.jsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useState } from 'react';
 
 const HelloPage = () => {
     return (
@@ -8,7 +8,20 @@ const HelloPage = () => {
                 {color:"red"}
             }>こんにちは</p>
             <h2>はじめてのGatsby</h2>
+            <ExampleComponent></ExampleComponent>
         </main>
+    )
+}
+
+function ExampleComponent() {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count+1)}>
+                Click me
+            </button>
+        </div>
     )
 }
 
