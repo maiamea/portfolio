1db69afa88b27796b515d67f33772631b8d59bb1
maiamea <maiameawork@gmail.com>
2021-11-12 14:53:13 +0900

    note記事一覧データをJSONで管理、note記事を日付降順表示に設定

--- src/components/NoteArticlesComponent.jsx
+++ src/components/NoteArticlesComponent.jsx
@@ -1,36 +1,13 @@
 import React from 'react';
 import {Li, DateStyle, ListTitle} from './ListStyleComponent';
+import articleLists from '../data/note.json';
 
-const articleLists = [
-  {
-    id: 1,
-    date: "2021/10/29",
-    title: "【参加メモ】 『SCRUM BOOT CAMP THE BOOK 増補改訂版』読書会 第3回目",
-    url: "https://note.com/maiamea/n/n21e44fdb4f10"
-  },
-  {
-    id: 2,
-    date: "2021/10/27",
-    title: "【参加メモ】 『SCRUM BOOT CAMP THE BOOK 増補改訂版』読書会 第2回目",
-    url: "https://note.com/maiamea/n/n792779910e0b"
-  },
-  {
-    id: 3,
-    date: "2021/10/25",
-    title: "【参加メモ】 『SCRUM BOOT CAMP THE BOOK 増補改訂版』読書会 第1回目",
-    url: "https://note.com/maiamea/n/n5cd7a8c0cbf7"
-  },
-  {
-    id: 4,
-    date: "2021/10/01",
-    title: "はじめてカンファレンスのセッション公募に応募したよ！",
-    url: "https://note.com/maiamea/n/n165d9f8199fa"
-  },
-];
+// 文字列の日付をDate型に変換してsortに使用
+const DescArticleLists = articleLists.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
 
 const NoteArticlesComponent = () => (
   <>
-    {articleLists.map((list) => (
+    {DescArticleLists.map((list) => (
       <ul>
         <Li key={list.id}>
           <a href={list.url}>
