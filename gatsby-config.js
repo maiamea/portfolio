1d83b65c533f661fabbf745f19819d3abfa1a921
maiamea <maiameawork@gmail.com>
2022-02-04 21:42:07 +0900

    fix: remove pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/portfolio`,
+  // pathPrefix: `/portfolio`,
   siteMetadata: {
     siteUrl: "https://maiamea.github.io/portfolio/",
     title: "maiamea's portfolio",
